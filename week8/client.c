4b3805025bfcdc1b7e748b06839cd51cf2d6ce0f
alexandder <aleksanderbolt@yahoo.com>
2014-04-16 22:17:17 +0200

    client

--- week8/client.c
+++ week8/client.c
@@ -13,32 +13,36 @@ int main(int argc, char const *argv[])
 	int connectStatus;
 	struct sockaddr_in server;
 
-	char buf[256];
+	int buf;
 
-	socketd = socket(AF_INET, SOCK_STREAM, 0);
+	socketd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (socketd == -1) {
 		perror("Socket creation");
 		exit(1);
 	}
 
 	server.sin_family = AF_INET;
-	inet_aton("153.19.1.202", &server.sin_addr);
-	server.sin_port = htons(5000);
+	server.sin_addr.s_addr = inet_addr("153.19.1.240");
+	server.sin_port = htons((ushort) 5000);
 
 	connectStatus = connect(socketd, (struct sockaddr*)&server, sizeof(server));
 	if (connectStatus == -1) {
-		printf("AAA\n");
 		perror("Connection error");
 		exit(1);
 	}
-	int number;
+	int slen = sizeof(server);
 	printf("Podaj liczbę do wysłania: ");
-	scanf("%d", &number);
-	write(socketd, &number, 4);
-	printf("\n");
-	read(socketd, &buf, 4);
-	printf("%s\n", buf);
+	scanf("%d", &buf);
+	printf("%d\n", buf);
+	int wres = sendto(socketd, (char *)&buf, sizeof(int), 0, (struct sockaddr *)&server, slen);
+       
 
-
+	//printf("write result: %d\n", wres);
+	
+	int rres = recvfrom(socketd, (char *)&buf, sizeof(int), 0, (struct sockaddr *)&server, &slen);
+	//printf("read result: %d\n", rres);
+	
+	printf("%d\n", buf);
+	close(socketd);
 	return 0;
 }
